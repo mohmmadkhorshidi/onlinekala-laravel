9e3336a36ca119c70f3b99150919e9c7e73f7bab
mohmmadkhorshidi <mohmmadkhorshidi78@gmail.com>
2024-11-21 00:59:26 +0330

    Changing the dashboard font

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
-    //
+    use HasFactory;
+//    protected $table = 'countries';
+    protected $guarded = [];
+
+    public function submit($formData)
+    {
+        Country::query()->create([
+            'name' => $formData['name'],
+        ]);
+    }
 }
--- resources/views/layouts/admin/scripts.blade.php
+++ resources/views/layouts/admin/scripts.blade.php
@@ -1,3 +1,4 @@
+<!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
 <script src="/admin/src/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="/admin/src/plugins/src/perfect-scrollbar/perfect-scrollbar.min.js"></script>
 <script src="/admin/src/plugins/src/mousetrap/mousetrap.min.js"></script>
@@ -8,3 +9,27 @@
 <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
 <script src="/admin/src/plugins/src/apex/apexcharts.min.js"></script>
 <script src="/admin/src/assets/js/dashboard/dash_2.js"></script>
+<script src="/admin/src/plugins/src/sweetalerts2/sweetalerts2.min.js"></script>
+<script src="/admin/src/plugins/src/sweetalerts2/custom-sweetalert.js"></script>
+<script>
+    window.addEventListener('success', function(event) {
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: event.detail,
+            showConfirmButton: false,
+            timer: 1500
+        })
+    })
+
+    window.addEventListener('error', function(event) {
+        Swal.fire({
+            position: 'center',
+            icon: 'error',
+            title: event.detail,
+            showConfirmButton: false,
+            timer: 3000
+        })
+    })
+</script>
+@stack('script')
