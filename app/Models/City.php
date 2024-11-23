fa33d82afa15e6c4f52d4b76c8305af10d8e3ef8
mohmmadkhorshidi <mohmmadkhorshidi78@gmail.com>
2024-11-23 12:45:02 +0330

    Complete city in admin panel

--- app/Models/City.php
+++ app/Models/City.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
-    //
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function submit($formData, $cityId)
+    {
+
+
+        City::query()->updateOrCreate([
+            'id' => $cityId
+        ],
+            [
+                'name' => $formData['name'],
+                'state_id' => $formData['stateId'],
+            ]);
+    }
+
+    public function state()
+    {
+        return $this->belongsTo(State::class);
+    }
 }
